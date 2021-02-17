a10be964d8cde6b13b9ebd7c826fdb212c00877d
Imane MEKIDECHE <i.mekideche@anem.dz>
2021-02-17 10:11:23 +0100

    commit ng-server project

---
+++ database/users.js
@@ -0,0 +1,34 @@
+var users = [
+  {
+    id: 1,
+    userName: "johnpapa",
+    firstName: "John",
+    lastName: "Papa"
+  },
+  {
+    id: 2,
+    userName: "bradgreen",
+    firstName: "Brad",
+    lastName: "Green"
+  },
+  {
+    id: 3,
+    userName: "igorminar",
+    firstName: "Igor",
+    lastName: "Minar"
+  },
+  {
+    id: 4,
+    userName: "martinfowler",
+    firstName: "Martin",
+    lastName: "Fowler"
+  },
+  {
+    id: 5,
+    userName: "imane",
+    firstName: "Imane",
+    lastName: "Mek",
+  }
+];
+
+module.exports = users;
